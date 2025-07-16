5e68d568ccd245a28a9c34287bacac2b9ebdf8a7
Khanhtungmtp <umlatao97@gmail.com>
2025-07-16 22:31:01 +0700

    DashboardChart

---
+++ LearnDashboardChart/LearnDashboardChart/frmMain.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearnDashboardChart
+{
+    public partial class frmMain : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            fluentDesignFormContainer1.Controls.Add(new ucDashboard { Dock=DockStyle.Fill});
+        }
+    }
+}
