a0330184be032c427d2fb17dbf413ef305c1e8d3
Khanhtungmtp <umlatao97@gmail.com>
2025-07-23 23:02:23 +0700

    Getting Started with the GridControl

---
+++ LearnGridControl/LearnGridControl/frmMain.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace LearnGridControl
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nORTHWNDDataSet = new LearnGridControl.NORTHWNDDataSet();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colProductID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colProductName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCategoryID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colQuantityPerUnit = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitPrice = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitsInStock = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitsOnOrder = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colReorderLevel = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDiscontinued = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.productsTableAdapter = new LearnGridControl.NORTHWNDDataSetTableAdapters.ProductsTableAdapter();
+            this.repositoryItemGridLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit();
+            this.repositoryItemGridLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.categoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.categoriesTableAdapter = new LearnGridControl.NORTHWNDDataSetTableAdapters.CategoriesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nORTHWNDDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.productsBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemGridLookUpEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(803, 411);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.nORTHWNDDataSet;
+            // 
+            // nORTHWNDDataSet
+            // 
+            this.nORTHWNDDataSet.DataSetName = "NORTHWNDDataSet";
+            this.nORTHWNDDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colProductID,
+            this.colProductName,
+            this.colCategoryID,
+            this.colQuantityPerUnit,
+            this.colUnitPrice,
+            this.colUnitsInStock,
+            this.colUnitsOnOrder,
+            this.colReorderLevel,
+            this.colDiscontinued});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsFind.AlwaysVisible = true;
+            this.gridView1.OptionsView.ShowAutoFilterRow = true;
+            // 
+            // colProductID
+            // 
+            this.colProductID.FieldName = "ProductID";
+            this.colProductID.Name = "colProductID";
+            this.colProductID.Visible = true;
+            this.colProductID.VisibleIndex = 0;
+            // 
+            // colProductName
+            // 
+            this.colProductName.FieldName = "ProductName";
+            this.colProductName.Name = "colProductName";
+            this.colProductName.Visible = true;
+            this.colProductName.VisibleIndex = 1;
+            // 
+            // colCategoryID
+            // 
+            this.colCategoryID.ColumnEdit = this.repositoryItemGridLookUpEdit1;
+            this.colCategoryID.FieldName = "CategoryID";
+            this.colCategoryID.Name = "colCategoryID";
+            this.colCategoryID.Visible = true;
+            this.colCategoryID.VisibleIndex = 2;
+            // 
+            // colQuantityPerUnit
+            // 
+            this.colQuantityPerUnit.FieldName = "QuantityPerUnit";
+            this.colQuantityPerUnit.Name = "colQuantityPerUnit";
+            this.colQuantityPerUnit.Visible = true;
+            this.colQuantityPerUnit.VisibleIndex = 3;
+            // 
+            // colUnitPrice
+            // 
+            this.colUnitPrice.FieldName = "UnitPrice";
+            this.colUnitPrice.Name = "colUnitPrice";
+            this.colUnitPrice.Visible = true;
+            this.colUnitPrice.VisibleIndex = 4;
+            // 
+            // colUnitsInStock
+            // 
+            this.colUnitsInStock.FieldName = "UnitsInStock";
+            this.colUnitsInStock.Name = "colUnitsInStock";
+            this.colUnitsInStock.Visible = true;
+            this.colUnitsInStock.VisibleIndex = 5;
+            // 
+            // colUnitsOnOrder
+            // 
+            this.colUnitsOnOrder.FieldName = "UnitsOnOrder";
+            this.colUnitsOnOrder.Name = "colUnitsOnOrder";
+            this.colUnitsOnOrder.Visible = true;
+            this.colUnitsOnOrder.VisibleIndex = 6;
+            // 
+            // colReorderLevel
+            // 
+            this.colReorderLevel.FieldName = "ReorderLevel";
+            this.colReorderLevel.Name = "colReorderLevel";
+            this.colReorderLevel.Visible = true;
+            this.colReorderLevel.VisibleIndex = 7;
+            // 
+            // colDiscontinued
+            // 
+            this.colDiscontinued.FieldName = "Discontinued";
+            this.colDiscontinued.Name = "colDiscontinued";
+            this.colDiscontinued.Visible = true;
+            this.colDiscontinued.VisibleIndex = 8;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // repositoryItemGridLookUpEdit1
+            // 
+            this.repositoryItemGridLookUpEdit1.AutoHeight = false;
+            this.repositoryItemGridLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemGridLookUpEdit1.DataSource = this.categoriesBindingSource;
+            this.repositoryItemGridLookUpEdit1.DisplayMember = "CategoryName";
+            this.repositoryItemGridLookUpEdit1.Name = "repositoryItemGridLookUpEdit1";
+            this.repositoryItemGridLookUpEdit1.PopupView = this.repositoryItemGridLookUpEdit1View;
+            this.repositoryItemGridLookUpEdit1.ValueMember = "CategoryID";
+            // 
+            // repositoryItemGridLookUpEdit1View
+            // 
+            this.repositoryItemGridLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.repositoryItemGridLookUpEdit1View.Name = "repositoryItemGridLookUpEdit1View";
+            this.repositoryItemGridLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.repositoryItemGridLookUpEdit1View.OptionsView.ShowGroupPanel = false;
+            // 
+            // categoriesBindingSource
+            // 
+            this.categoriesBindingSource.DataMember = "Categories";
+            this.categoriesBindingSource.DataSource = this.nORTHWNDDataSet;
+            // 
+            // categoriesTableAdapter
+            // 
+            this.categoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 411);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nORTHWNDDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private NORTHWNDDataSet nORTHWNDDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private NORTHWNDDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colProductID;
+        private DevExpress.XtraGrid.Columns.GridColumn colProductName;
+        private DevExpress.XtraGrid.Columns.GridColumn colCategoryID;
+        private DevExpress.XtraGrid.Columns.GridColumn colQuantityPerUnit;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitPrice;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitsInStock;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitsOnOrder;
+        private DevExpress.XtraGrid.Columns.GridColumn colReorderLevel;
+        private DevExpress.XtraGrid.Columns.GridColumn colDiscontinued;
+        private DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit repositoryItemGridLookUpEdit1;
+        private DevExpress.XtraGrid.Views.Grid.GridView repositoryItemGridLookUpEdit1View;
+        private System.Windows.Forms.BindingSource categoriesBindingSource;
+        private NORTHWNDDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter;
+    }
+}
---
+++ LearnGridControl/LearnGridControl/frmMain.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearnGridControl
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'nORTHWNDDataSet.Categories' table. You can move, or remove it, as needed.
+            this.categoriesTableAdapter.Fill(this.nORTHWNDDataSet.Categories);
+            // TODO: This line of code loads data into the 'nORTHWNDDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.nORTHWNDDataSet.Products);
+
+        }
+    }
+}
